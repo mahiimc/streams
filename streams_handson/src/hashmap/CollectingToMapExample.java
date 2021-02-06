1720631d423c5f56ebcfbe8cf85967f51a25edde
Mahesh Chary <imciiit@gmail.com>
2021-02-06 20:35:58 +0530

    Collecting to Map --> Student as a Key and List<Hobbies> as Value

---
+++ streams_handson/src/hashmap/CollectingToMapExample.java
@@ -0,0 +1,20 @@
+package hashmap;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import repository.Student;
+import repository.StudentRespository;
+
+public class CollectingToMapExample {
+	
+	public static void main(String[] args) {
+		List<Student> students = new StudentRespository().getStudents();
+		Map<String,List<String>> studentsHobbies = students.stream()
+													.collect(Collectors.toMap(Student :: getName, Student :: getHobbies));
+		System.out.println(studentsHobbies);
+		
+	}
+
+}
