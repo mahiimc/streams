414d8a4a7d2dadc97c38e63ead864b9286d9f37b
Mahesh Chary <imciiit@gmail.com>
2021-02-06 15:29:22 +0530

    Filter Example :: Code added

--- streams_handson/src/streams_handson/filter/FilterExample.java
+++ streams_handson/src/streams_handson/filter/FilterExample.java
@@ -1,5 +1,33 @@
 package streams_handson.filter;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FilterExample {
+	//simple Example with numbers
+	public static void main(String[] args) {
+		List<Integer> nums = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+		
+		// Filter Even nums from the Collection of Integers
+	
+		/**
+		 * 1. Create Stream of nums
+		 * 2. Apply Filter
+		 * 3. Collect Filtered Items into a new List
+		 */
+		
+		List<Integer> evenNums = nums.stream() //step1
+		.filter(n -> n%2 == 0) //step2
+		.collect(Collectors.toList()); //step 3
+		@SuppressWarnings("unused")
+		List<Integer> oddNums = nums.stream()
+				.filter(n ->n%2 != 0)
+				.collect(Collectors.toList());
+		//oddNums.forEach(System.out::println);
+		evenNums.forEach(System.out::println); // Method Reference
+		
+		
+	}
 
 }
