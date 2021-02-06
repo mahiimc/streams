b168340008bfba2912267ee10693c6cc0e92df83
Mahesh Chary <imciiit@gmail.com>
2021-02-06 20:13:52 +0530

    Minor Changes

--- streams_handson/src/flatmap/StudentFlatMapExample.java
+++ streams_handson/src/flatmap/StudentFlatMapExample.java
@@ -4,14 +4,13 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import repository.Student;
+import repository.StudentRespository;
+
 public class StudentFlatMapExample {
 	
 	public static void main(String[] args) {
-		Student student1 = new Student(1,"abc",Arrays.asList("Hob1","Hob2","Hob3","Hob4"));
-		Student student2 = new Student(2,"xyz",Arrays.asList("Hob6","Hob7","Hob8","Hob9"));
-		Student student3 = new Student(3,"abcde",Arrays.asList("Hob1","Hob11","Hob12","Hob13"));
-		Student student4 = new Student(4,"someexperson",Arrays.asList("Hob14","Hob15","Hob16","Hob17"));
-		List<Student> students = Arrays.asList(student1,student2,student3,student4);
+		List<Student> students = new StudentRespository().getStudents();
 		List<String> hobbies = students.stream()
 										.flatMap(stu -> stu.getHobbies().stream())
 										.distinct() // removes duplicate data
--- streams_handson/src/repository/Student.java
+++ streams_handson/src/repository/Student.java
@@ -1,4 +1,4 @@
-package flatmap;
+package repository;
 
 import java.util.List;
 
@@ -7,10 +7,12 @@ public class Student {
 	private int id;
 	private String name;
 	private List<String> hobbies;
-	public Student(int id, String name, List<String> hobbies) {
+	private int noOfBacklogs;
+	public Student(int id, String name, List<String> hobbies,int noOfBacklogs) {
 		this.id=id;
 		this.name=name;
 		this.hobbies=hobbies;
+		this.noOfBacklogs = noOfBacklogs;
 	}
 	public int getId() {
 		return id;
@@ -20,5 +22,8 @@ public class Student {
 	}
 	public List<String> getHobbies() {
 		return hobbies;
+	}
+	public int getNoOfBacklogs() {
+		return noOfBacklogs;
 	}
 }
