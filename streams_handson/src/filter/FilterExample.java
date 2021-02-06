8249c4634a568bd1daaff1726484e9ea25ceba78
Mahesh Chary <imciiit@gmail.com>
2021-02-06 20:16:43 +0530

    Filter Example

--- streams_handson/src/filter/FilterExample.java
+++ streams_handson/src/filter/FilterExample.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 public class FilterExample {
 	//simple Example with numbers
 	public static void main(String[] args) {
-		List<Integer> nums = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+		List<Integer> nums = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11);
 		
 		// Filter Even nums from the Collection of Integers
 	
